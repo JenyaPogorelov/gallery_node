3a5778a80a417d0674f46d26ca5ebc91de9b46cd
ShadowD <jeckcame@gmail.com>
2022-02-18 22:17:12 +0300

    Добавление картинок

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const {resolve} = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: resolve(__dirname, 'dist'),
+        filename: "main.[contenthash].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: "file-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: resolve(__dirname, 'src', 'index.html')
+        })
+    ]
+}
